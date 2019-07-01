1c7c643279d3aabd97a7a77d780ccbe7c0afd9b3
jeffyjkang <jeffyjkang@gmail.com>
2019-07-01 16:35:19 -0700

    update naming convention to be same

--- api/routes/adminRoute.js
+++ api/routes/adminRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const adminDb = require("../helpers/adminDb");
+const adminDb = require("../helpers/adminModel");
 
 //authorization middleware
 const { authenticate } = require("../../auth/restricted-middleware");
