6f023bc236844abdcdcb8b2db0f0247eb6858d84
Dionuta <diounta@gmail.com>
2019-07-02 13:32:40 -0700

    update to candidate route

--- api/helpers/candidateModel.js
+++ api/helpers/candidateModel.js
@@ -1,6 +1,44 @@
 const db = require("../../data/dbConfig");
+
 module.exports = {
-  get: () => {
-    return db("candidate");
+   find,
+   findById,
+   add,
+   remove,
+   update,
+   
+  };
+
+  
+function find() {
+    return db('candidate');
+  };
+
+function findById(id) {
+    return db('candidate')
+    .where({ id })
+}
+
+
+async function add(candinfo) {
+    const [id] = await db('candidate').insert(candinfo);
+  
+    return findById(id);
   }
-};
+
+
+  function remove(id) {
+    return db('candidate')
+      .where({ id })
+      .del();
+  }
+
+  function update(id, changes) {
+    return db('candidate')
+      .where({ id })
+      .update(changes, '*');
+  }
+
+
+
+  
